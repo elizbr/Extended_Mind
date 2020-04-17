185fa3503ca9ae314e2a7f016f5793c4c755cc5b
Elizabeth Lloyd Bradford <elizbr@umich.edu>
2020-04-17 18:59:08 -0400

    updated

--- extended_mind.py
+++ extended_mind.py
@@ -3,6 +3,7 @@
 #imports
 from spotipy.oauth2 import SpotifyClientCredentials
 import sys
+import os
 import spotipy
 import spotipy.util as util
 import secrets
@@ -11,72 +12,113 @@ from bs4 import BeautifulSoup
 import lxml
 import pitchfork
 import sqlite3
+import json
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials #To access authorised Spotify data
+
+
+os.environ["SPOTIPY_CLIENT_ID"] = secrets.spotify_id
+os.environ["SPOTIPY_CLIENT_SECRET"] = secrets.spotify_secret
+
+
+#classes 
+class Album:
+    '''an album
+
+    Instance Attributes 
+    Title - str 
+    Label - str 
+    Editorial - str
+    Cover - str 
+    Year - str 
+    Rating - float 
+    Artist - str 
+    '''
+    def __init__(self, title = 'None', label = 'None', edit = 'None', cover = 'None', year = 0000, rating = 0.0, artist = 'None'):
+        self.title = title 
+        self.label = label 
+        self.edit = edit 
+        self.cover = cover 
+        self.year = year
+        self.rating = rating 
+        self.artist = artist 
+
+    def jjson(self):
+        '''returns a list of content
+        '''
+        json_safe_t = [self.title, self.label, self.edit,self.cover, self.year, self.rating,self.artist]
+        return  json_safe_t
+
+class Artist:
+    '''an artist
+
+    Instance Attributes:
+    '''
+    def __init__(self, name = 'None', genre = 'None', bio = 'None', topalbum = 'None', toptracks = 'None', albumlist = 'None'):
+        self.name = name 
+        self.genre = genre
+        self.bio = bio 
+        self.topalbum = topalbum
+        self.toptracks = toptracks
+        self.albumlist = albumlist
+
+    def jjson(self):
+        '''returns a list of content
+        '''
+        json_safe_t = [self.name, self.genre, self.bio, self.topalbum, self.toptracks, self.albumlist]
+        return json_safe_t
+    
 
 #reference
+# https://medium.com/@RareLoot/extracting-spotify-data-on-your-favourite-artist-via-python-d58bc92a4330
 # https://github.com/michalczaplinski/pitchfork
 
 
-"""
-export SPOTIPY_CLIENT_ID = secrets.spotify_id
-export SPOTIPY_CLIENT_SECRET= secrets.spotify_secret
-export SPOTIPY_REDIRECT_URI= 'http://localhost/?code='
+# set up cache
+CACHE_FILENAME = 'cache.json'
+CACHE_DICT = {}
 
-client_credentials_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-util.prompt_for_user_token(username = secrets.spotify_nm,
-                           scope = 'user-read-private',
-                           client_id= SPOTIPY_CLIENT_ID,
-                           client_secret= SPOTIPY_CLIENT_SECRET,
-                           redirect_uri='http://localhost/?code=')
-
-
-#scrape spotify for artist 
-def get_artist(name):
-    '''borrowed code; 
+def open_cache():
+    ''' Opens the cache file if it exists and loads the JSON into
+    the CACHE_DICT dictionary.
+    If the cache file doesn't exist, creates a new cache dictionary
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    dict
+        The opened cache
     '''
-    results = sp.search(q='artist:' + name, type='artist')
-    items = results['artists']['items']
-    if len(items) > 0:
-        return items[0]
-    else:
-        return None
-"""
-
-#scrape pitchfork for album reviews 
-
-
-#scrape spotify with cache
-
-
-#album query string from spotify to pitchfork
-def format_album_query(album):
-    '''takes string album and formats into query string
-    parameter <str> - sentence case album title 
-    return <str> - formatted query string
-    '''
-    try: 
-        query = ''
-        if ' ' in album.strip():
-            step_one = album.lower().split(' ')
-            for word in step_one:
-                query = query + '%20' + word
-        else:
-            query = album.lower()
+    try:
+        cache_file = open(CACHE_FILENAME, 'r')
+        cache_contents = cache_file.read()
+        cache_dict = json.loads(cache_contents)
+        cache_file.close()
     except:
-        query = album.lower()
-    return f"https://pitchfork.com/search/?query={query}"
+        cache_dict = {}
+    return cache_dict
 
-'''
-#
-'''
-#scrape pitchfork with cache 
+def save_cache(cache_dict):
+    ''' Saves the current state of the cache to disk
+    
+    Parameters
+    ----------
+    cache_dict: dict
+        The dictionary to save
+    
+    Returns
+    -------
+    None
+    '''
+    dumped_json_cache = json.dumps(cache_dict)
+    fw = open(CACHE_FILENAME,"w")
+    fw.write(dumped_json_cache)
+    fw.close()
 
-
-# create dbs
-
-
-#1. Create the database and its tables
+# Create the database & its tables
 def create_db():
     conn = sqlite3.connect('albums.sqlite')
     cur = conn.cursor()
@@ -86,11 +128,11 @@ def create_db():
     create_album_sql = '''
         CREATE TABLE IF NOT EXISTS "Albums" (
 	    "Id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	    "AlbumTitle"	TEXT UNIQUE,
+	    "AlbumTitle"	TEXT,
 	    "AlbumLabel"	TEXT,
-	    "AlbumEditorial"	TEXT UNIQUE,
-	    "AlbumCover"	TEXT UNIQUE,
-        "Year"      TEXT,
+	    "AlbumEditorial"	TEXT,
+	    "AlbumCover"	TEXT,
+        "Year"      NUMERIC,
 	    "AlbumScore"	NUMERIC,
 	    "ArtistTitle"	TEXT)
         '''
@@ -116,6 +158,43 @@ def create_db():
     conn.close()
 
 
+
+# spotify call
+
+
+client_id = secrets.spotify_id
+client_secret = secrets.spotify_secret
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) #spotify object to access API
+name = "radiohead" #chosen artist
+result = sp.search(name) #search query
+print(result['tracks']['items'][0]['artists'])
+
+
+#Extract Artist's uri
+artist_uri = result['tracks']['items'][0]['artists'][0]['uri']
+#Pull all of the artist's albums
+sp_albums = sp.artist_albums(artist_uri, album_type='album')
+#Store artist's albums' names' and uris in separate lists
+album_names = []
+album_uris = []
+for i in range(len(sp_albums['items'])):
+    album_names.append(sp_albums['items'][i]['name'])
+    album_uris.append(sp_albums['items'][i]['uri'])
+    
+print(album_names)
+
+
+
+#scrape pitchfork for album reviews 
+
+
+#scrape spotify with cache
+
+
+#scrape pitchfork with cache 
+
+
 #2. Extract data records from the CSV and Web API. 
 def album_scrape_p(artist, album):
     '''takes artist and album and creates dict for each 
@@ -124,24 +203,69 @@ def album_scrape_p(artist, album):
     _artist <str> - Artist Name
     _album <album name> - Artist's specificed album 
     Returns:
-    _album_content <list> - contains all info 
+    _album_content <Album> - contains all info 
     necessary for db add 
     '''
     p = pitchfork.search(artist, album) # the title is autocompleted
-    album = p.album() # the full album title
+    x = p.year()
+    if len(x) != 4:
+        try: 
+            y = x.split('-').strip()
+            x = y[0]
+            x = int(x)
+        except:
+            x = 0000
+    
+    g = p.editorial()
+    mf = g.replace('/','')
+
+        
+    title = p.album() # the full album title
     label = p.label()
-    editorial = p.editorial()
-    score = p.score()
+    edit = mf
+    rating = p.score()
     artist = p.artist()
-    year = p.year()
+    year = int(x)
     try: 
         cover = p.cover()
     except:
         cover = 'None'
-    album_content = (album, label, editorial, cover, year, score, artist,)
-    return album_content
+    return Album(title= title, label = label, edit= edit, cover = cover, year = year, rating = rating, artist = artist)
 
 
+def result_obj(artist, album):
+    obj = f"{artist}_{album}"
+    return obj 
+
+def album_scrape_cache(result_obj, artist, album):
+    '''Check the cache for a saved result for this baseurl+params:values
+    combo. If the result is found, return it. Otherwise send a new 
+    request, save it, then return it.
+    
+    Parameters
+    ----------
+    endpoint_url: string
+        API endpoint URL 
+    result_obj: dict
+        Dictionary of returned info
+    
+    Returns
+    -------
+    dict
+        query results in JSON / dict 
+    '''
+    if result_obj in CACHE_DICT.keys():
+        print("Using Cache")
+        y = CACHE_DICT[result_obj]
+        return Album(title=y[0], label=y[1], edit = y[2], cover = y[3], year = y[4], rating = y[5], artist = y[6])
+    else:
+        print("Fetching")
+        content = album_scrape_p(artist, album)
+        CACHE_DICT[result_obj] = content.jjson()
+        #print(content.title)
+        save_cache(CACHE_DICT)
+        add_album_to_sql(content.jjson())
+        return CACHE_DICT[result_obj]
 
 #3. Insert each extracted record into the DB, making sure that the 
 #relationships between Bars and Countries are correctly represented.
@@ -151,14 +275,13 @@ def add_album_to_sql(cont):
         cur = conn.cursor()
         print("Successfully Connected to SQLite")
 
-        insert_query = f"""INSERT INTO Albums
-        (AlbumTitle, AlbumLabel, AlbumEditorial, AlbumCover, Year, AlbumScore, ArtistTitle) 
-        VALUES {cont}"""
+        insert_query = f'''INSERT INTO Albums
+        (AlbumTitle, AlbumLabel, AlbumCover, Year, AlbumScore, ArtistTitle) 
+        VALUES ("{cont[0]}", "{cont[1]}", "{cont[3]}", {cont[4]}, {cont[5]}, "{cont[6]}")'''
 
         cur.execute(insert_query)
         conn.commit()
-        print("Record inserted successfully into SqliteDb_developers table ", cursor.rowcount)
-        cur.close()
+        conn.close()
 
     except:
         print("Failed to insert data into sqlite table")
@@ -176,73 +299,70 @@ def add_album_to_sql(cont):
 
 
 # User input for artist 
-
-
 #flask interfact for user 
+"""
+from flask import Flask, render_template, request
+import sqlite3
+import plotly.graph_objects as go
+
+app = Flask(__name__)
+"""
+
+"""
+album = 'Sunbather'
+artist = 'Deafheaven'
+cont = album_scrape_p(artist, album).jjson()
+#print(x.edit)
+print(f'''INSERT INTO Albums
+        (AlbumTitle, AlbumLabel, AlbumCover, Year, AlbumScore, ArtistTitle) 
+        VALUES ("{cont[0]}", "{cont[1]}", "{cont[3]}", "{cont[4]}", "{cont[5]}", "{cont[6]}")''')
+"""
+
+
+import spotipy
+import spotipy.util as util
+scope = 'user-library-read'
+
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print("Usage: %s username" % (sys.argv[0],))
+    sys.exit()
+
+token = util.prompt_for_user_token(username, scope)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_saved_tracks()
+    for item in results['items']:
+        track = item['track']
+        print(track['name'] + ' - ' + track['artists'][0]['name'])
+else:
+    print("Can't get token for", username)
+
+"""def get_artist(name):
+    results = sp.search(q='artist:' + name, type='artist')
+    items = results['artists']['items']
+    if len(items) > 0:
+        return items[0]
+    else:
+        return None"""
+
+name = "radiohead" #chosen artist
+result = sp.search(name) #search query
+print(result['tracks']['items'][0]['artists'])
+#x = get_artist("Radiohead")
+#print(x)
 
 ################
-create_db()
-v = album_scrape_p('Deafheaven', 'Sunbather')
-#print(v)
-xxx = ('High Violet', '4AD', 
-'''The National became popular in a very traditional way: 
-by releasing some really good albums, then touring the hell out of them. 
-They\'re boilerplate indie, free of hot new genre tags or feature-ready 
-backstories, which is something their detractors derive great joy from 
-pointing out. If the National are important, rather than merely good, 
-it\'s for writing about the type of lived-in moments that rock bands 
-usually don\'t write about that well. The characters in National songs 
-have real jobs, have uninteresting sex, get drunk, and lie to one another. 
-They do so during the regular course of a workaday week, on Tuesdays and 
-Wednesdays. The National aren\'t "dad-rock" so much as "men\'s magazine 
-rock": music chiefly interested in the complications of being a stable 
-person expected to own certain things and dress certain ways.\nOn the Nat
-ional\'s fifth album, High Violet, those constraints are starting to wear
- on them, which makes a lot of sense: they wear on most people. In betwee
- n patches of obtuse imagery, singer Matt Berninger sounds increasingly s
- elf-destructive. The record\'s upbeat numbers don\'t cheer him up so muc
- h as commiserate with him. All of this makes High Violet a dark affair, 
- even for a band with a reputation for sad-bastard melodrama. The National
- ve never sounded triumphant, but they can still be reassuring, with Be
- rninger\'s\xa0lyrics acting as salves for our own neuroses. Six drink
- s in, tired of your coworkers, wishing you could just go home and lau
- gh at sitcoms with someone? Maybe get laid? The National\'s got your
-  back.\nWith an ever rising profile and plenty of indie-famous friends-
-  - Sufjan Stevens and Bon Iver\'s Justin Vernon guest here-- the Nat
-  ional were afforded the opportunity to obsess over High Violet. They c
-  ould\'ve holed up and recorded an idiosyncratic, expectation-defying mes
-  s. Instead they produced an ornate, fussed-over record that sounds like 
-  no one other than themselves. Given the amount of flack they take for b
-  eing a no-frills bore, simply refining their sound was arguably the brav
-  er option. They miss, occasionally-- the string-drenched closer, "Vande
-  rlyle Crybaby Geeks", is too decadent for its own good-- but mostly, t
-  hey construct gorgeous, structurally sound vignettes. There are few b
-  ands that could craft a song like "Sorrow"-- in which emotion acts as 
-  a character and the band turns Berninger\'s balladry into a well paced 
-  jog-- without stumbling over their own ambitions. The guitars on "Afra
-  id of Everyone" actually sound nervous; "England" speaks of cathedral
-  s over properly magisterial drums. These are triumphs of form.\nBernin
-  ger is still, for the most part, a socially obsessed claustrophobe. He 
-  has upper-class guilt on "Lemonworld" ("Cousins and cousins somewhere o
-  verseas/ But it\'ll take a better war to kill a college man like me," "T
-  his pricey stuff makes me dizzy"). "Bloodbuzz Ohio"\'s magnificent choru
-  s ("I still owe money/ To the money/ To the money I owe") addresses the 
-  familiar, harrowing financial burdens of adulthood. He\'s best when he t
-  ones down the angst in favor of reflection or confusion. High Violet see
-  ms less likely to engender the personal connections of Boxer, but it\'s 
-  also bigger and more engaging-- a possibly offputting combination for a 
-   band following the footsteps of Echo and the Bunnymen, Wilco, and Arcad
-   e Fire. After all, eagerness often trumps execution, and the National a
-   ren\'t immune: For his part, Berninger looks increasingly like Dos Equi
-   s\' Most Interesting Man in the World, and his cryptic lyrics seem like
-    an application for the title.\nBut the National rarely miss; when they
-     aim for powerful or poetic, they get there. High Violet is the sound 
-     of a band taking a mandate to be a meaningful rock band seriously, an
-     d they play the part so fully that, to some, it may be off-putting. B
-     ut these aren\'t mawkish, empty gestures; they\'re anxious, personal 
-     songs projected onto wide screens.\xa0Even if you don\'t consider you
-     rself an upwardly mobile stiff with minor social anxiety, the Nationa
-     l make it sound grand, confusing, and relatable.\n''', 
-     '2010', 'None', 
-     8.7, 'The National')
-add_album_to_sql(v)
+"""
+if __name__ == "__main__":
+    CACHE_DICT = open_cache()
+    create_db()
+    artist = input(f"What is your artist pick?")
+    album = input("album: ")
+
+    obj = result_obj(artist, album)
+    v = album_scrape_cache(obj, artist, album)
+        
+"""
